95763fc29e8b6ce988c714ad17300ec97ca9a6f3
jamesona <jamesonaranda@gmail.com>
2019-12-03 13:40:23 -0700

    making messages async

--- system-messages.js
+++ system-messages.js
@@ -1,24 +1,26 @@
-function randomNumber(max, min = 0) {
-	min = Math.ceil(min)
-	max = Math.floor(max)
-	return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-export const headerMessage = `ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM` +
-		`\nCOPYRIGHT 2075-2077 ROBCO INDUSTRIES\n-Server ${randomNumber(64, 1)}-`
+export const messages = (async () => {
+	function randomNumber(max, min = 0) {
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
 	
-export const trespasserMessage = `-RobCo Tresspasser Management System-` +
-	`\n=====================================`
-
-export const hack = `\nRobcOS v.85` +
-	`\n(C)2076 RobCo` +
-	`\n========================` +
-	`\n| User Log:` +
-	`\n| >> Administrator (RobCoID 2398-H)` +
-	'\n| >> New_Admin: null' +
-	'\n| Welcome new user, null' +
-	'\n| >> New_Targeting_Param:' +
-	'\n| >>> null_userGroup' +
-	`\n========================`
+	export const headerMessage = `ROBCO INDUSTRIES UNIFIED OPERATING SYSTEM` +
+			`\nCOPYRIGHT 2075-2077 ROBCO INDUSTRIES\n-Server ${randomNumber(64, 1)}-`
+		
+	export const trespasserMessage = `-RobCo Tresspasser Management System-` +
+		`\n=====================================`
 	
-export const scaleBar = `|12345679|12345679|12345679|12345679|12345679|12345679|12345679|12345679`
+	export const hack = `\nRobcOS v.85` +
+		`\n(C)2076 RobCo` +
+		`\n========================` +
+		`\n| User Log:` +
+		`\n| >> Administrator (RobCoID 2398-H)` +
+		'\n| >> New_Admin: null' +
+		'\n| Welcome new user, null' +
+		'\n| >> New_Targeting_Param:' +
+		'\n| >>> null_userGroup' +
+		`\n========================`
+		
+	export const scaleBar = `|12345679|12345679|12345679|12345679|12345679|12345679|12345679|12345679`
+})()
