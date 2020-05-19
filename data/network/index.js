6ba453ca2cdf2a3e716684fb7ac7ab600f5a8a7f
jamesona <jamesonaranda@gmail.com>
2020-05-18 19:33:52 -0600

    fix more paths

--- data/network/index.js
+++ data/network/index.js
@@ -1,5 +1,4 @@
 export const network = {
-	'localhost': '/data/network/localhost.js',
-	'mdpl-13': '/data/network/mdpl-13.js'
-
+	'localhost': '../data/network/localhost.js',
+	'mdpl-13': '../data/network/mdpl-13.js'
 }
